640920dbfc2201ec3c3aeb8fc933306fbea07f48
mwmh1012 <mwmh1012@gmail.com>
2026-08-04 15:54:46 +0900

    Update tideTime.js

--- api/tideTime.js
+++ api/tideTime.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const { obs_post_id, date } = req.query;
   const key = process.env.DATA_GO_KR_KEY;
-  const url = `https://apis.data.go.kr/1192136/tideFcstTime?serviceKey=${key}&ObsCode=${obs_post_id}&Date=${date}&resultType=json`;
+  const url = `https://apis.data.go.kr/1192136/tideFcstTime/GetTideFcstTimeApiService?serviceKey=${key}&type=json&numOfRows=30&pageNo=1&obsCode=${obs_post_id}&reqDate=${date}&min=60`;
   try {
     const r = await fetch(url);
     const data = await r.json();
