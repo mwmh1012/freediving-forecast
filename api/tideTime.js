ad7bc43b7f3428995222fe1d4ad768d42187016a
mwmh1012 <mwmh1012@gmail.com>
2026-08-04 15:47:00 +0900

    Create tideTime.js

---
+++ api/tideTime.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const { obs_post_id, date } = req.query;
+  const key = process.env.DATA_GO_KR_KEY;
+  const url = `https://apis.data.go.kr/1192136/tideFcstTime?serviceKey=${key}&ObsCode=${obs_post_id}&Date=${date}&resultType=json`;
+  try {
+    const r = await fetch(url);
+    const data = await r.json();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: String(e) });
+  }
+}
