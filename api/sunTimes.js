5547ac7743b17c94c41eacc7204dd6d4e4a76792
mwmh1012 <mwmh1012@gmail.com>
2026-08-05 13:59:23 +0900

    Update sunTimes.js

--- api/sunTimes.js
+++ api/sunTimes.js
@@ -30,6 +30,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(200).json(data);
   } catch (e) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.status(500).json({ error: String(e) });
   }
 }
